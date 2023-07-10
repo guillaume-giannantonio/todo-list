f34712bb1c1accfcae2651a7c7bff394416a62ec
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-07-10 19:20:25 +0200

    Add header

--- src/display/initial.js
+++ src/display/initial.js
@@ -1,4 +1,9 @@
 export default function () {
 	const content = document.createElement('div')
+	const header = document.createElement('header')
+	const h1 = document.createElement('h1')
+	h1.textContent = 'TODO LIST'
+	header.appendChild(h1)
+	content.appendChild(header)
 	return content
 }
